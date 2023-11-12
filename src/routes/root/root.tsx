22699eb92fc257c5350334828ac2451073fa158f
Joey Yu <joey@jyu.dev>
2023-11-11 21:57:42 -0500

    small fix

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,7 +1,12 @@
 import "@/index.css";
+import { Outlet } from "@tanstack/react-router";
 
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
